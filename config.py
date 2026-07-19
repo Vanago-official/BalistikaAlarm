437d48e52771f0f89e837c7a38efc59ff4515c71
Vanago-official <vananovoselov824@gmail.com>
2026-07-19 05:49:17 +0300

    Improved list of words trigger

--- config.py
+++ config.py
@@ -8,4 +8,12 @@ API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 CHANNELS_TO_MONITOR = ["tviy_kyiv", "kiev1", -1003740116044, -1004298960926]
-TARGET_WORDS = ["балістик", "тривог", "ракет"]
+TARGET_WORDS = [
+    "балістик",
+    "циркон",
+    "кинджал",
+    "кинжал",
+    "іскандер",
+    "швидкісн",
+    "ракет",
+]
